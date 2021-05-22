28f631c88a9351f9d1324223dd1cf2c13e9b15dd
Joseph Garcia <jgarcia@netskope.com>
2021-05-22 12:17:56 -0700

    made the first steps

--- first_prjoject.py
+++ first_prjoject.py
@@ -1,1 +1,17 @@
+import random
+#1 ask for user input location / age / height
+#2 take input and put into random sentence generator
+#3 output input
 
+#input
+print('what is your location?')
+location = input()
+print('what is your age?')
+age = input()
+print('what is your height?')
+height = input()
+
+#dictionary
+random_dict = {"castle", "food", "title", "window", "boxes", "kittycat", "never"}
+#print
+print (random_dict)
